a7bd2dbe4be2b421f00ea572b6eb1f96c6caac34
KevFR <ikevfr@gmail.com>
2026-08-23 11:18:01 +0200

    responsive update part 2

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ function toggleMenu() {
     // Ajoute/enlève la classe sur le body pour tout masquer
     document.body.classList.toggle("menu-open");
 }
+
+document.querySelectorAll('nav #myLinks a').forEach(link => {
+    link.addEventListener('click', function () {
+        document.querySelectorAll('nav #myLinks a').forEach(l => l.classList.remove('active'));
+
+        // Ajoute la classe active au lien cliqué
+        this.classList.add('active');
+    });
+});
